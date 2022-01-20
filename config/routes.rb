a75037b1f7a7ea2b4453549e8bce8d4c368d9e14
carlos diaz <diazmelian.carlos@gmail.com>
2022-01-20 17:49:16 +0000

    view task details

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: "tasks#home"
   get "tasks", to: "tasks#tasks"
   get "tasks/new", to: "tasks#new"
-  get "tasks/:id", to: "tasks#show"
+  get "tasks/:id", to: "tasks#show", as: :task
   get "tasks/:id/edit", to: "tasks#edit"
 end
